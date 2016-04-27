b2088f235b707188c2ae38b803369a36c7e52bed
huro3h_satoshi <huro3h@gmail.com>
2016-04-27 15:38:35 +0800

    init commit

--- sampleSlider/ViewController.swift
+++ sampleSlider/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func sliderChanged(sender: UISlider) {
+        // スライダーの値が変わった時
+        // print("スライダのつまみが動いたよ！")
+        // sender:イベントを発生を発生させている部品そのもの
+        print(sender.value)
+        //.valueはこの場合つまみの位置の意
+        
+        // TODO:音量(sender.value)が0.5より小さい場合、「音が小さいです、それ以外の場合「音が大きいですとデバッグエリアに表示させてください。」
+        
+        if sender.value < 0.5{
+            print("「音が小さいです！」")
+        }else{
+            print("「音が大きいです！」")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
