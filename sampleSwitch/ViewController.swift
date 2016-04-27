a59dc005e26ef02d1021965d355147131fc2f9c3
huro3h_satoshi <huro3h@gmail.com>
2016-04-27 15:37:54 +0800

    init commit

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -15,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func switchChanged(sender: UISwitch) {
+        
+        // print("スイッチが切り替わったよ！")
+        // sender:イベントを発生させている部品そのもの
+        // print(sender.on)
+        
+        // TODO:スイッチON時に「スイッチオン」、OFF時に「スイッチオフ」をデバッグエリアに表示するif文表示
+        if sender.on{
+            print("スイッチオン")
+        }else{
+            print("スイッチオフ")
+        }
+        // sender.onにboolan型(TorF)が入っているので
+        // 上記のような書き方でもOK!
+        // ちなみに.onは指定できるけど、.offはないよ！
+        
+        // 解答例
+        if sender.on == true{
+            print("すいっちおん")
+        }else{
+            print("すいっちおふ")
+        }
+    
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
