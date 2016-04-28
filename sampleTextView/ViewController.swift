16082b20a7c24081c57e1019b6bb2473e6531337
huro3h_satoshi <huro3h@gmail.com>
2016-04-28 13:59:52 +0800

    4.28-4

--- sampleTextView/ViewController.swift
+++ sampleTextView/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // textプロパティに文言を代入している。
+        myTextView.text = "こんにちは Hello,World!"
+        
+        // 文字色を変えてみる(オブジェクト名.プロパティ名)
+        // UIColor 色を変えるオブジェクト名.blueColor関数()を呼び出している
+        myTextView.textColor = UIColor.blueColor()
+        
+        // フォントを指定する
+        myTextView.font = UIFont(name: "AmericanTypewriter", size: 20)
+        
+        // 文字寄せ位置（デフォルトは左寄せ）
+        myTextView.textAlignment = NSTextAlignment.Center
+        
+        // TODO:Helloを10回表示する(PHPのfor文)
+        for var i=1 i <=10 i++ {
+            myTextView.text = myTextView + "Hello"
+        }
     }
 
     override func didReceiveMemoryWarning() {
