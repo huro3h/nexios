e213caa58c9967676d652636bae5e21ef36a5494
huro3h_satoshi <huro3h@gmail.com>
2016-04-28 14:21:47 +0800

    for文

--- sampleTextView/ViewController.swift
+++ sampleTextView/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // textプロパティに文言を代入している。
-        myTextView.text = "こんにちは Hello,World!"
+        myTextView.text = "Hello,World!"
         
         // 文字色を変えてみる(オブジェクト名.プロパティ名)
         // UIColor 色を変えるオブジェクト名.blueColor関数()を呼び出している
@@ -28,9 +28,24 @@ class ViewController: UIViewController {
         myTextView.textAlignment = NSTextAlignment.Center
         
         // TODO:Helloを10回表示する(PHPのfor文)
-        for var i=1 i <=10 i++ {
-            myTextView.text = myTextView + "Hello"
+        for var i = 1; i <= 10; i++ {
+            // 2回表示されないのは、元から入っているhello-が「上書き」されるから！
+            myTextView.text = myTextView.text + "Hello"
         }
+        
+        // TODO:こんにちはn(改行 → \r)で10個メッセージを表示
+        // 変数も改行も""の中に入れてやること！変数は()付で！
+        var num = 1
+        for var i = 1; i <= 10; i++ {
+            myTextView.text = myTextView.text + "こんにちは\(num)\r"
+            num++
+        }
+        
+        // 回答例
+//        for var n = 1; n <= 10; n++ {
+//            myTextView.text = myTextView.text + "こんにちは\(n)\r"
+//        }
+
     }
 
     override func didReceiveMemoryWarning() {
