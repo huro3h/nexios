f82789be54050831dc23103e82a75c6c9912032d
huro3h_satoshi <huro3h@gmail.com>
2016-04-28 14:43:11 +0800

    選択肢を日付のみにセット myDatePicker.datePickerMode = UIDatePickerMode.Date

--- sampleDatePicker/ViewController.swift
+++ sampleDatePicker/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // 名前をつける時には上の方へ、アクションは下へ
+    @IBOutlet weak var myDatePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 選択肢を日付のみにセット
+        myDatePicker.datePickerMode = UIDatePickerMode.Date
+        
     }
 
     override func didReceiveMemoryWarning() {
