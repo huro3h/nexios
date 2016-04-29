f3f638633fcd74465dd9cee4dd3700ff4d82e668
huro3h_satoshi <huro3h@gmail.com>
2016-04-29 14:12:13 +0800

    アクションシートを表示

--- sampleActionSheet/ViewController.swift
+++ sampleActionSheet/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	
+	@IBAction func tapBtn(sender: UIButton) {
+		// アクションシートを作成
+		var alertController = UIAlertController(
+		title: "タイトル",
+		message: "メッセージ",
+		// ここを.ActionSheetにする↓
+		preferredStyle: .ActionSheet)
+		
+		// 通常ボタンを追加
+		alertController.addAction(UIAlertAction(
+			title: "アクション",
+			style: .Default,
+			handler: { action in print("OK")}))
+		
+		// アクションシートを表示
+		presentViewController(alertController, animated: true, completion: nil)
+		}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
