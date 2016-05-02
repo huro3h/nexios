f6c912921fed6d3e733e016da7e543051f29c6a9
huro3h_satoshi <huro3h@gmail.com>
2016-05-02 15:22:46 +0800

    ユーザーデフォルト〜即反映

--- sampleUserDefault/ViewController.swift
+++ sampleUserDefault/ViewController.swift
@@ -14,7 +14,46 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	@IBAction func tapReturn(sender: UITextField) {
+		
+		// ユーザーデフォルトを用意する(情報の保管場所を用意)
+		var myDefault = NSUserDefaults.standardUserDefaults()
+		
+		// データを書き込んで
+		myDefault.setObject(sender.text, forKey: "myString")
+		
+		// 即反映させる(すぐ使いたい時はこの手順を踏む)
+		myDefault.synchronize()
+		
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
