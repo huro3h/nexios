01b20ed6d6d9c2055738c722068efccf90768569
huro3h_satoshi <huro3h@gmail.com>
2016-05-03 13:29:16 +0800

    タップしたら　タップされたよ！　と表示

--- sampleTapRecognizer/ViewController.swift
+++ sampleTapRecognizer/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	@IBAction func tapView(sender: UITapGestureRecognizer) {
+		
+		print("タップされたよ！")
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
