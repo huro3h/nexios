07ed2656b52ab0e2b95a944ee7ffcba86d8244e1
huro3h_satoshi <huro3h@gmail.com>
2016-05-03 15:32:18 +0800

    自分で作った .json 読む所まで

--- sampleJSON001/ViewController.swift
+++ sampleJSON001/ViewController.swift
@@ -12,9 +12,44 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	// 画面が表示される時に
+	override func viewWillAppear(animated: Bool) {
+		// json.txtを読み込んで
+		var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+		var jsondata = NSData(contentsOfFile: path!)
+		
+		// 配列データに変換して
+		let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as!
+		NSArray
+		
+		// 配列の個数だけ繰り返して表示する
+		for dat in jsonArray {
+			print ("値=[\(dat)]")
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
