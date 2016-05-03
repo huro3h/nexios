d4b15f07985b6e31b61c9d9890daf7a5d02c2b52
huro3h_satoshi <huro3h@gmail.com>
2016-05-03 14:21:57 +0800

    twitterでつぶやくよテスト

--- sampleSocialButton/ViewController.swift
+++ sampleSocialButton/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
+// import 各種フレームワークを使えるようにする
+// SocialでSNS機能を使えるように
+import Social
+
 class ViewController: UIViewController {
 
+	@IBOutlet weak var twitterBtn: UIButton!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	@IBAction func tapTwitterBtn(sender: UIButton) {
+		// print("おされたよ")
+		
+		var twitterVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+		twitterVC.setInitialText("twitterでつぶやくよてすと")
+		presentViewController(twitterVC, animated: true, completion: nil)
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
