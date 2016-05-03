464ac5b36060904b52a90928bf40f99664a3c869
huro3h_satoshi <huro3h@gmail.com>
2016-05-03 15:48:27 +0800

    詳細追加

--- sampleJSON001/ViewController.swift
+++ sampleJSON001/ViewController.swift
@@ -16,15 +16,19 @@ class ViewController: UIViewController {
 
 	// 画面が表示される時に
 	override func viewWillAppear(animated: Bool) {
-		// json.txtを読み込んで
+		
+		// json.txtを読み込んで(22行目で止めるとpathに何が入っているかわかる)
+		// NSBundle.mainBundle()自分が動かしているアプリの場所を返してくれる
 		var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+		
+		// 数字の羅列(読めない)状態で代入
 		var jsondata = NSData(contentsOfFile: path!)
 		
-		// 配列データに変換して
+		// 配列データに変換するよ(読めるデータにして返す)
 		let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as!
 		NSArray
 		
-		// 配列の個数だけ繰り返して表示する
+		// 配列の個数だけ繰り返して表示する(for in 高速列挙で datに代入)
 		for dat in jsonArray {
 			print ("値=[\(dat)]")
 		}
