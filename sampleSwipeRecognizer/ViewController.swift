6f3f3f58f60636919bcbfa8e7128bc980b0aa202
huro3h_satoshi <huro3h@gmail.com>
2016-05-03 14:03:54 +0800

    "うえにすわいぷされました"

--- sampleSwipeRecognizer/ViewController.swift
+++ sampleSwipeRecognizer/ViewController.swift
@@ -14,7 +14,36 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
+	// 右上の選択欄からどの方向からスワイプするか
+	// 指定が可能
+	@IBAction func swipeView(sender: UISwipeGestureRecognizer) {
+		print("スワイプされました")
+	}
+	
+	// TODO:色つきのビューを追加して、上スワイプを感知し、
+	// "上スワイプされました"と表示しましょう。
+	
+	@IBAction func swipeViewUP(sender: UISwipeGestureRecognizer) {
+		print("うえにすわいぷされました")
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
