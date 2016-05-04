686de61d0f4d9bd275cb9de9f067425c9b8f06a3
huro3h_satoshi <huro3h@gmail.com>
2016-05-04 18:36:38 +0800

    init commit 多次元配列

--- sampleJSON003/ViewController.swift
+++ sampleJSON003/ViewController.swift
@@ -14,7 +14,45 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
+	override func viewWillAppear(animated: Bool) {
+		// json.txtファイルの読み込み
+		let path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+		
+		// 内容を取り出す
+		let jsondata = NSData(contentsOfFile: path!)
+		
+		// 辞書データに変換
+		// as データ型で指定したデータ型に変換(ダウンキャスト)
+		let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSDictionary
+		
+		// 辞書データの個数だけ繰り返して表示する(この場合2回:理由以下に記載)
+		for (key, data) in jsonDictionary {
+			var d1 = data["餅"] as! String
+			var d2 = data["醤油"] as! String
+			var d3 = data["月見団子"] as! String
+			
+			// この場合キーには"関東"と"関西"が入っている為
+			print("キー[\(key)] 餅=[\(d1)]")
+			print("キー[\(key)] 醤油=[\(d2)]")
+			print("キー[\(key)] 月見団子=[\(d3)]")
+			// json.txtを記号で値を揃えると読み込んだ...けどなんで(・ω・)？
+		}
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
