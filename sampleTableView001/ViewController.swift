77a7a55f8a644d0040e94205bbd59f91fa6805f2
huro3h_satoshi <huro3h@gmail.com>
2016-05-06 14:34:25 +0800

    デリゲート処理3つ、間違えやすいので要注意！

--- sampleTableView001/ViewController.swift
+++ sampleTableView001/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 // ここでエラーが出るのは、{}内に必要なコード2つ書かれていない為
+// あとデリゲートで2つつけるのを忘れないように注意！
+// (Delegate処理2つの意)
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
 	@IBOutlet weak var myTableView: UITableView!
@@ -25,11 +27,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 	}
 	
 	// 2.行に表示する内容をリセット
+	// returnで入る物、int型(引数) -> 戻り値のデータ型
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		
+		// cell テーブルビューのもう一つ中にあるメソッド
+		// reuseIdentifier 再利用できますよーの引数
 		var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+		// cellの中に表示する字を代入している (indexPath.row)今何行目のデータを出そうとしているか。の情報が.row（行番号）に入っている引数
 		cell.textLabel!.text = "\(indexPath.row)行目"
 		return cell
+	}
+	
+	// 3.選択された時に行う処理(Delegate処理)
+	// 似た名前がたくさんあるので間違えないように注意！
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		print("\(indexPath.row)行目を選択")
 	}
 	
 	
