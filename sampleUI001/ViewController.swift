ae7b125081e82a4a798a0f364fd210fc6d521879
huro3h_satoshi <huro3h@gmail.com>
2016-05-10 16:44:57 +0800

    宿題途中まで

--- sampleUI001/ViewController.swift
+++ sampleUI001/ViewController.swift
@@ -9,16 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var aView = UIView(frame: CGRectMake(0,0,100,100))
+	
+	// 起動画面サイズの取得
+	let myBoundSize:CGSize = UIScreen.mainScreen().bounds.size
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	
+	self.aView.backgroundColor = UIColor.greenColor()
+	self.aView = UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))
+	self.view.addSubview(aView)
+	
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func tapBtn(sender: UIButton) {
+//		print("tapされた！")
+		if (self.aView == UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))){
+			UIView.animateWithDuration(0.8,
+				animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
+					self.aView.frame = CGRectMake(0, 0, 100, 100)}
+		} else {
+			UIView.animateWithDuration(0.8,
+				animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
+					self.aView.frame = CGRectMake(0, -250, 100, 100)}
+		}
+		
+	
 	}
+	
+	
+	
+	
+	
+	
+	
+//	override func didReceiveMemoryWarning() {
+//		super.didReceiveMemoryWarning()
+//		// Dispose of any resources that can be recreated.
+//	}
 
 
 }
