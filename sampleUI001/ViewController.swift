606b6e4085fdafaa0e8fad51c60b3c71115ca46a
huro3h_satoshi <huro3h@gmail.com>
2016-05-11 13:00:21 +0800

    flag使わず完成させようとしたが、ボタンが動かんかった。。(´・ω・`)

--- sampleUI001/ViewController.swift
+++ sampleUI001/ViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 	
 	var aView = UIView(frame: CGRectMake(0,0,100,100))
+	@IBOutlet weak var myBtn: UIButton!
+
 	
 	// 起動画面サイズの取得
 	let myBoundSize:CGSize = UIScreen.mainScreen().bounds.size
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	
@@ -21,22 +24,32 @@ class ViewController: UIViewController {
 	self.aView = UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))
 	self.view.addSubview(aView)
 	
+//	self.myBtn = UIButton!(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))
+//	self.view.addSubview(myBtn);
+	
 	}
 
 	@IBAction func tapBtn(sender: UIButton) {
-//		print("tapされた！")
-		if (self.aView == UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))){
-			UIView.animateWithDuration(0.8,
-				animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
-					self.aView.frame = CGRectMake(0, 0, 100, 100)}
+		// print("tapされた！")
+		if (self.aView.frame == CGRectMake(0,self.myBoundSize.height,self.myBoundSize.width,250)){
+			UIView.animateWithDuration(0.5,
+			animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
+			self.aView.frame = CGRectMake(0,self.myBoundSize.height-250,self.myBoundSize.width,250)},
+//			self.myBtn.frame = CGRectMake(self.myBtn.frame.origin.x,self.myBtn.frame.origin.y+250,self.myBtn.frame.width,self.myBtn.frame.height),
+//			UIView.animateWithDuration(0.5,
+//			animations: { () -> Void in
+//				self.myBtn.frame = CGRectMake(0,self.myBoundSize.height-250,self.myBoundSize.width,250)},
+			completion: {finished in print("出る！")})
 		} else {
-			UIView.animateWithDuration(0.8,
-				animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
-					self.aView.frame = CGRectMake(0, -250, 100, 100)}
+			UIView.animateWithDuration(0.5,
+			animations: { () -> Void in self.aView.backgroundColor = UIColor.greenColor()
+			self.aView.frame = CGRectMake(0,self.myBoundSize.height,self.myBoundSize.width,250)},
+			completion: {finished in print("隠れる！")})
 		}
-		
+	
 	
 	}
+	
 	
 	
 	
@@ -48,7 +61,7 @@ class ViewController: UIViewController {
 //		super.didReceiveMemoryWarning()
 //		// Dispose of any resources that can be recreated.
 //	}
-
-
 }
+
+
 
