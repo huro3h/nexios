85511564146f904f49d8f67c971f3ca3924c3ca5
huro3h_satoshi <huro3h@gmail.com>
2016-05-23 13:53:54 +0800

    about Notification

--- samplePushNotification/ViewController.swift
+++ samplePushNotification/ViewController.swift
@@ -16,7 +16,26 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	@IBOutlet weak var tapBtn: UIButton!
+
+	@IBAction func tapBtn(sender: UIButton) {
+		// ローカル通知の設定
+		let notification : UILocalNotification = UILocalNotification()
+		
+		// タイトル
+		notification.alertTitle = "Fire"
+		
+		// 通知メッセージ
+		notification.alertBody = "ファイヤー！"
+		
+		// Timezoneの設定
+		notification.timeZone = NSTimeZone.defaultTimeZone()
+		
+		// 10秒後に設定
+		notification.fireDate = NSDate(timeIntervalSinceNow: 3)
+		
+		// Notificationを登録する
+		UIApplication.sharedApplication().scheduleLocalNotification(notification)
+	}
 	
 	
 	override func didReceiveMemoryWarning() {
