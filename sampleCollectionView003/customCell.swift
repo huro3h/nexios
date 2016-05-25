789b0670a3060dec55708448622dd4aafbd97dae
huro3h_satoshi <huro3h@gmail.com>
2016-05-25 14:49:07 +0800

    init commit (collection復習と非同期の仕組み)

---
+++ sampleCollectionView003/customCell.swift
@@ -0,0 +1,15 @@
+//
+//  customCell.swift
+//  sampleCollectionView003
+//
+//  Created by satoshiii on 2016/05/25.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+//
+
+import UIKit
+// 4-3 customcellのそれぞれのパーツにoutlet名付け
+
+class customCell: UICollectionViewCell {
+	@IBOutlet weak var image: UIImageView!
+	@IBOutlet weak var title: UILabel!
+}
