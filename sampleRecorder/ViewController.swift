144f3796b064086a50f16cfcc248aaf54ee479cb
huro3h_satoshi <huro3h@gmail.com>
2016-05-26 14:18:19 +0800

    // 5.再生機能の関数 // 6.下で作った再生に関する自作関数 と、各箇所の注意点

--- sampleRecorder/ViewController.swift
+++ sampleRecorder/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  sampleRecorder
 //
@@ -23,8 +22,6 @@ class ViewController: UIViewController {
 	var audioPlayer: AVAudioPlayer?
 	let fileName = "sample.caf" // .caf 音声ファイルに用いられる
 	
-	
-	
 	@IBOutlet weak var recordBtn: UIButton!
 	@IBOutlet weak var plyBtn: UIButton!
 	
@@ -35,10 +32,13 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func tapBtnRecord(sender: UIButton) {
+		audioRecorder?.record()
 	}
 	
 	
 	@IBAction func tapBtnPlay(sender: UIButton) {
+		// 6.下で作った再生に関する自作関数
+		play()
 	}
 
 	// 自作関数(録音する為に必要な設定を行う関数で、今回はviewDidLoad時に行う)
@@ -54,18 +54,38 @@ class ViewController: UIViewController {
 			AVNumberOfChannelsKey : 2,
 			AVSampleRateKey : 44100
 		]
-		do{
-			try audioRecorder = AVAudioR
-		} catch {
 		
+		do{
+			// settings: recordSettingの引数で設定が必要になる為
+			// 先に上で設定している。
+			try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+		} catch {
+			print("エラーがでました...!")
 		}
 	}
+	
+	// 5.再生機能の関数
+	func play(){
+		do{
+			// audioPlayer?.play()をここで書かない理由
+			// ここで書くと、読み込み時のエラーか、他のエラーかの判別がつかなくなる為
+			try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+		}catch{
+			print("再生時にえらーでたよー！")
+		}
+		// ↓の一文が無いと音が出ないよ
+		audioPlayer?.play()
+		
+	}
+	
 	
 	// 4.録音するファイルのパスを取得(録音時、再生時に参照)
 	// swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
 	func documentFilePath()-> NSURL{
+		// ファイルパスを配列の形で取得する、配列[0]番目にディレクトリパスを代入
 		let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
 		let dirURL = urls[0]
+		// print(urls)
 		return dirURL.URLByAppendingPathComponent(fileName)
 	}
 	
@@ -79,10 +99,8 @@ class ViewController: UIViewController {
 	
 	
 	
-	//	override func didReceiveMemoryWarning() {
+//	override func didReceiveMemoryWarning() {
 //		super.didReceiveMemoryWarning()
 //	}
-
-
 }
 
