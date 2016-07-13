be0705d5f1e8eaa441738ce05f1cd20eac1e25f5
huro3h_satoshi <huro3h@gmail.com>
2016-07-13 19:32:40 +0900

    no message

--- sampleCollectionView003/ViewController.swift
+++ sampleCollectionView003/ViewController.swift
@@ -79,14 +79,15 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
 	
 	// 4.何アイテム表示させるか → 4-2 customcell.swift新しくファイル追加
 	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//		return 21
+		//return 21
+		return musicList.count
 		
 		// 取ってくる件数が21件より多ければ問題は無いが、
 		// 21件を下回った場合、表示するデータがなくなる為エラーが発生する
 		// musicList.countとすることで、取ってきた件数 = returnの値となる為
 		// 上記のケースが発生しなくなり、エラーが起こらなくなる
 		
-		return musicList.count
+		
 	}
 	
 	// 5.cellForItemAtIndexPath -> cellの内容を作る
