933b5141d7ca41c64e068904d39393f867c54c99
huro3h_satoshi <huro3h@gmail.com>
2016-10-12 15:34:18 +0900

    swift2.3 -> 3.0

--- sampleJSON001/ViewController.swift
+++ sampleJSON001/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
 	}
 
 	// 画面が表示される時に
-	override func viewWillAppear(animated: Bool) {
+	override func viewWillAppear(_ animated: Bool) {
 		
 		// json.txtを読み込んで(22行目で止めるとpathに何が入っているかわかる)
 		// NSBundle.mainBundle()自分が動かしているアプリの場所を返してくれる
-		var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+		var path = Bundle.main.path(forResource: "json", ofType: "txt")
 		
 		// 数字の羅列(読めない)状態で代入
 		var jsondata = NSData(contentsOfFile: path!)
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
 		// 配列データに変換するよ(読めるデータにして返す)
 		// try!(例外処理: エラーが起こった時に()で囲んだ部分を処理する =nilにして返す)
 		// 結果変なエラーが起こりにくくなる [Swift2.0〜]
-		let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as!
+		let jsonArray = (try! JSONSerialization.jsonObject(with: jsondata! as Data, options: [])) as!
 		NSArray
 		
 		// 配列の個数だけ繰り返して表示する(for in 高速列挙で datに代入)
